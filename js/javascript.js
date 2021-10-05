a29ebf5099fb1ef3b4b4fa363a3007a8760fac31
Angeline Polidano <angelinepolidano@gmail.com>
2021-10-05 16:04:53 +1100

    html&css

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+// single player, doubles
+
+
+  $("button").click(function() {
+      if(turn == 1) {
+          $("#screen").text("PLAYER 2 TURN FOLLOWS");
+
+// function to check winning move.
+
+
+
+
+
+
+});
