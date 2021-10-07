ceec48e1b18061fec53fdbe07af57eb871c02dc7
Angeline Polidano <angelinepolidano@gmail.com>
2021-10-07 12:11:08 +1100

    Disable function to clear the game board

--- js/javascript.js
+++ js/javascript.js
@@ -15,7 +15,7 @@ const wins = [
 ];
 const moves = [];
 let currentPlayer = "X";
-
+let markCell = 0
 // This is my Change Player function
   function changePlayer() {
   if( currentPlayer  === "X" )
@@ -25,8 +25,10 @@ let currentPlayer = "X";
     }
 
 $('.game--board').click(function(e) {
-  // console.log('clicked');
+  console.log('clicked');
 $(e.target).text(currentPlayer);
+markCell = (markCell + 1);
+console.log(markCell);
 checkForWin(e.target);
 changePlayer();
   });
@@ -35,72 +37,105 @@ function checkForWin(element) {
 console.log($(element).data('button-index'));
 const buttonNumber = Number($(element).data('button-index'));
 console.log(buttonNumber);
-let winner = [];
+// let winner = []
 
 moves[buttonNumber] = currentPlayer;
 console.log(moves[0]);
 
 if(moves[0]==="X"&&moves[1]==="X"&&moves[2]==="X"||moves[0]==="0"&&moves[1]==="0"&&moves[2]==="0") {
-  console.log('win!');
+ $('.game--result').text("Winner!");
+// disabled ();
+// alert ('win!');
+
 }else if
 (moves[3]==="X"&&moves[4]==="X"&&moves[5]==="X"||moves[3]==="0"&&moves[4]==="0"&&moves[5]==="0") {
-  console.log('win!');
+// $('.button').prop('disabled', true);
+$('.game--result').text("Winner!");
+// disabled ();
+  // console.log('win!');
 }else if
 (moves[6]==="X"&&moves[7]==="X"&&moves[8]==="X"||moves[6]==="0"&&moves[7]==="0"&&moves[8]==="0") {
-  console.log('win!');
+// $('.button').prop('disabled', true);
+$('.game--result').text("Winner!");
+// disabled ();
+  // console.log('win!');
 }else if
 (moves[0]==="X"&&moves[3]==="X"&&moves[6]==="X"||moves[0]==="0"&&moves[3]==="0"&&moves[6]==="0") {
-  console.log('win!');
+// $('.button').prop('disabled', true);
+$('.game--result').text("Winner!");
+// disabled ();
+// $('.game--result).text("Winner!");
+  // console.log('win!');
 }else if
 (moves[1]==="X"&&moves[4]==="X"&&moves[7]==="X"||moves[1]==="0"&&moves[4]==="0"&&moves[7]==="0") {
-  console.log('win!');
+// $('.button').prop('disabled', true);
+$('.game--result').text("Winner!");
+// disabled ();
+// $('.game--result).text("Winner!");
+  // console.log('win!');
 }else if
 (moves[2]==="X"&&moves[5]==="X"&&moves[8]==="X"||moves[2]==="0"&&moves[5]==="0"&&moves[8]==="0") {
-  console.log('win!');
+// $('.button').prop('disabled', true);
+$('.game--result').text("Winner!");
+// disabled ();
+// $('.game--result).text("Winner!");
+  // console.log('win!');
 }else if
 (moves[0]==="X"&&moves[4]==="X"&&moves[8]==="X"||moves[0]==="0"&&moves[4]==="0"&&moves[8]==="0") {
-  console.log('win!');
+// $('.button').prop('disabled', true);
+$('.game--result').text("Winner!");
+// disabled ();
+// $('.game--result).text("Winner!");
+  // console.log('win!');
 }else if
 (moves[6]==="X"&&moves[4]==="X"&&moves[2]==="X"||moves[6]==="0"&&moves[4]==="0"&&moves[2]==="0") {
-  console.log('win!');
-} else {
-  console.log("cat's game! MEOW!");
+// $('.button').prop('disabled', true);
+$('.game--result').text("Winner!");
+// disabled ();
+// $('.game--result).text("Winner!");
+  // console.log('win!');
 }
 
+ else if (markCell === 9) {
+$('.game--result').text("cat's game! MEOW!");
+// disabled ();
+// $('.game--result).text("cat's game! MEOW!");
+  // console.log();
+}
 
-
-
-
-
-
-// NEXT STEPS
-//diasble board - when the condition is met then disable board.
-// be sure to git commit with each change from now
-//add correct indentation
-// alert players on winner rather than just console.log
-//button for instructions have them in a side pannel....
-// have a nicer colored background.
-//CREATE A SCORE CARD
-//increment the score by 1
-// function call the disable board funtion after each each round.( each else if statement)
-//player score variable...
-// on click new game
-// $('.instructions').click(function () {
-//     alert(instructions);
-// });
-//
+ if ($(".game--result").text().includes("Winner"||"cat's game! MEOW!")) {
+            $('.button').off("click");
+            $('.game--board').off("click");
+        }
 
 
 };
 
-$('#restart').click( function() {
+$('#restart').click( function () {
   console.log('click');
   $('.button').text("");
-$('.game--status').text("");
+$('.game--result').text("");
 
+$('.button').on("click");
+$('.game--board').on("click");
+ changePlayer ();
 })
 
 
-
-
+//doc ready close out
 });
+
+// NEXT STEPS
+//score card
+//store winner in a variable for the score card.
+// if $('.game--board') === "XXX" ||"000"||
+//increment the winners'score by 1
+ //can I put a boolean function in here too?
+//diasble board - when the condition is met then disable board.
+// be sure to git commit with each change from now
+//add correct indentation
+//button for instructions have them in a side pannel....
+// $('.instructions').click(function () {
+//     alert(instructions);
+// });
+//
