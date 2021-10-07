b1e7ac8b4d1bd5a3e6b06888b0910e1c0360fde9
Angeline Polidano <angelinepolidano@gmail.com>
2021-10-07 13:20:47 +1100

    working version with window reload

--- js/javascript.js
+++ js/javascript.js
@@ -15,7 +15,7 @@ const wins = [
 ];
 const moves = [];
 let currentPlayer = "X";
-let markCell = 0
+let markButton = 0
 // This is my Change Player function
   function changePlayer() {
   if( currentPlayer  === "X" )
@@ -27,8 +27,8 @@ let markCell = 0
 $('.game--board').click(function(e) {
   console.log('clicked');
 $(e.target).text(currentPlayer);
-markCell = (markCell + 1);
-console.log(markCell);
+markButton = (markButton + 1);
+console.log(markButton);
 checkForWin(e.target);
 changePlayer();
   });
@@ -44,91 +44,69 @@ console.log(moves[0]);
 
 if(moves[0]==="X"&&moves[1]==="X"&&moves[2]==="X"||moves[0]==="0"&&moves[1]==="0"&&moves[2]==="0") {
  $('.game--result').text("Winner!");
-// disabled ();
-// alert ('win!');
 
 }else if
 (moves[3]==="X"&&moves[4]==="X"&&moves[5]==="X"||moves[3]==="0"&&moves[4]==="0"&&moves[5]==="0") {
-// $('.button').prop('disabled', true);
 $('.game--result').text("Winner!");
-// disabled ();
-  // console.log('win!');
+
 }else if
 (moves[6]==="X"&&moves[7]==="X"&&moves[8]==="X"||moves[6]==="0"&&moves[7]==="0"&&moves[8]==="0") {
-// $('.button').prop('disabled', true);
 $('.game--result').text("Winner!");
-// disabled ();
-  // console.log('win!');
+
 }else if
 (moves[0]==="X"&&moves[3]==="X"&&moves[6]==="X"||moves[0]==="0"&&moves[3]==="0"&&moves[6]==="0") {
-// $('.button').prop('disabled', true);
 $('.game--result').text("Winner!");
-// disabled ();
-// $('.game--result).text("Winner!");
-  // console.log('win!');
+
 }else if
 (moves[1]==="X"&&moves[4]==="X"&&moves[7]==="X"||moves[1]==="0"&&moves[4]==="0"&&moves[7]==="0") {
-// $('.button').prop('disabled', true);
 $('.game--result').text("Winner!");
-// disabled ();
-// $('.game--result).text("Winner!");
-  // console.log('win!');
+
 }else if
 (moves[2]==="X"&&moves[5]==="X"&&moves[8]==="X"||moves[2]==="0"&&moves[5]==="0"&&moves[8]==="0") {
-// $('.button').prop('disabled', true);
 $('.game--result').text("Winner!");
-// disabled ();
-// $('.game--result).text("Winner!");
-  // console.log('win!');
+
 }else if
 (moves[0]==="X"&&moves[4]==="X"&&moves[8]==="X"||moves[0]==="0"&&moves[4]==="0"&&moves[8]==="0") {
-// $('.button').prop('disabled', true);
 $('.game--result').text("Winner!");
-// disabled ();
-// $('.game--result).text("Winner!");
-  // console.log('win!');
+
 }else if
 (moves[6]==="X"&&moves[4]==="X"&&moves[2]==="X"||moves[6]==="0"&&moves[4]==="0"&&moves[2]==="0") {
-// $('.button').prop('disabled', true);
 $('.game--result').text("Winner!");
-// disabled ();
-// $('.game--result).text("Winner!");
-  // console.log('win!');
 }
 
- else if (markCell === 9) {
+ else if (markButton === 9) {
 $('.game--result').text("cat's game! MEOW!");
-// disabled ();
-// $('.game--result).text("cat's game! MEOW!");
-  // console.log();
 }
 
  if ($(".game--result").text().includes("Winner"||"cat's game! MEOW!")) {
-            $('.button').off("click");
-            $('.game--board').off("click");
-        }
+ $('.button').off("click");
+ $('.game--board').off("click");
+}
 
 
 };
 
 $('#restart').click( function () {
-  console.log('click');
-  $('.button').text("");
-$('.game--result').text("");
-
-$('.button').on("click");
-$('.game--board').on("click");
- changePlayer ();
+  window.location.reload()
+// console.log('click');
+// $('.button').text("");
+// $('.game--result').text("");
+//
+// $('.button').on("click");
+// $('.game--board').on("click");
+// changePlayer ();
 })
 
 
 //doc ready close out
 });
 
+
+
+
 // NEXT STEPS
 //score card
 //store winner in a variable for the score card.
-// if $('.game--board') === "XXX" ||"000"||
 //increment the winners'score by 1
  //can I put a boolean function in here too?
 //diasble board - when the condition is met then disable board.
