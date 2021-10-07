dfce25903a5f509dfcc10b742823ae02a79d562b
Angeline Polidano <angelinepolidano@gmail.com>
2021-10-07 20:57:32 +1100

    working game with score card

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Special Thanks to Rowena and Pat for their help!
 
-Here is the [play tic-tac-toe](https://angepol.github.io/tic-tac-toe/).
+Play [ tic-tac-toe](https://angepol.github.io/tic-tac-toe/).
--- js/javascript.js
+++ js/javascript.js
@@ -1,102 +1,151 @@
 $(document).ready(function() {
-   // These are my Global Variables
-   // const wins = [
-   //
-   //    [0, 1, 2],
-   //    [3, 4, 5],
-   //    [6, 7, 8],
-   //    [0, 3, 6],
-   //    [1, 4, 7],
-   //    [2, 5, 8],
-   //    [0, 4, 8],
-   //    [6, 4, 2]
-   //
-   // ];
-   const moves = [];
-   let currentPlayer = "X";
-   let markButton = 0
-   // This is my Change Player function
-   function changePlayer() {
-      if (currentPlayer === "X") {
-         currentPlayer = "0";
-      } else {
-         currentPlayer = "X";
-      }
-      return;
-   }
+  // These are my Global Variables
+  let playerOne = 0;
+  let playerTwo = 0;
+  let moves = [];
+  let currentPlayer = "X";
+  let markButton = 0;
+  // This is my Change Player function
+  function changePlayer() {
+    console.log('change player is running');
+    if (currentPlayer === "X") {
+      console.log('it thinks player is X');
+      currentPlayer = "0";
+    } else {
+      currentPlayer = "X";
+    }
+    return;
+  }
 
-   $('.button').click(function(e) {
+  const buttonRefresh = function() {
+    $('.button').off("click");
+    currentPlayer = "X";
+    $('.button').click(function(e) {
+      // console.log('change player is running');
       console.log('clicked');
-      // if ($('.button').html()!= ""){
-      //   return;
-      // }
+
+
       $(e.target).text(currentPlayer);
       markButton = (markButton + 1);
       console.log(markButton);
       checkForWin(e.target);
       changePlayer();
-      // if ($('.button').html() != "") {
-      //    return;
-      // }
 
-   });
+    });
+  }
 
-
-   function checkForWin(element) {
-      console.log($(element).data('button-index'));
-      const buttonNumber = Number($(element).data('button-index'));
-      console.log(buttonNumber);
+  function checkForWin(element) {
+    console.log($(element).data('button-index'));
+    const buttonNumber = Number($(element).data('button-index'));
+    console.log(buttonNumber);
 
 
-      moves[buttonNumber] = currentPlayer;
-      console.log(moves[0]);
+    moves[buttonNumber] = currentPlayer;
+    console.log(moves[0]);
 
-      if (moves[0] === "X" && moves[1] === "X" && moves[2] === "X" || moves[0] === "0" && moves[1] === "0" && moves[2] === "0") {
-         $('.game--result').text("Winner!");
+    if (moves[0] === "X" && moves[1] === "X" && moves[2] === "X") {
+      playerOne = playerOne + 1
+      $('.game--result').text("Player one has won!");
+      $('.playerOne').text(`Player One: ${playerOne}`);
+      // $('.PlayerOne').text(`Score ${playerOne}`);
 
-      } else if (moves[3] === "X" && moves[4] === "X" && moves[5] === "X" || moves[3] === "0" && moves[4] === "0" && moves[5] === "0") {
-         $('.game--result').text("Winner!");
+    } else if ( moves[0] === "0" && moves[1] === "0" && moves[2] === "0") {
+      playerTwo = playerTwo + 1
+      $('.game--result').text("Player two has won!");
+      $('.playerTwo').text(`Player Two: ${playerTwo}`);
 
-      } else if (moves[6] === "X" && moves[7] === "X" && moves[8] === "X" || moves[6] === "0" && moves[7] === "0" && moves[8] === "0") {
-         $('.game--result').text("Winner!");
+    } else if (moves[3] === "X" && moves[4] === "X" && moves[5] === "X")  {
+      playerOne = playerOne + 1
+      $('.game--result').text("Player one has won!");
+      $('.playerOne').text(`Player One: ${playerOne}`);
 
-      } else if (moves[0] === "X" && moves[3] === "X" && moves[6] === "X" || moves[0] === "0" && moves[3] === "0" && moves[6] === "0") {
-         $('.game--result').text("Winner!");
+    } else if (moves[3] === "0" && moves[4] === "0" && moves[5] === "0") {
+      playerTwo = playerTwo + 1
+      $('.game--result').text("Player two has won!");
+        $('.playerTwo').text(`Player Two: ${playerTwo}`);
 
-      } else if (moves[1] === "X" && moves[4] === "X" && moves[7] === "X" || moves[1] === "0" && moves[4] === "0" && moves[7] === "0") {
-         $('.game--result').text("Winner!");
+    } else if (moves[6] === "X" && moves[7] === "X" && moves[8] === "X") {
+      playerOne = playerOne + 1
+      $('.game--result').text("Player one has won!");
+      $('.playerOne').text(`Player One: ${playerOne}`);
 
-      } else if (moves[2] === "X" && moves[5] === "X" && moves[8] === "X" || moves[2] === "0" && moves[5] === "0" && moves[8] === "0") {
-         $('.game--result').text("Winner!");
+    } else if (moves[6] === "0" && moves[7] === "0" && moves[8] === "0") {
+      playerTwo = playerTwo + 1
+      $('.game--result').text("Player two has won!");
+      $('.playerTwo').text(`Player Two: ${playerTwo}`);
 
-      } else if (moves[0] === "X" && moves[4] === "X" && moves[8] === "X" || moves[0] === "0" && moves[4] === "0" && moves[8] === "0") {
-         $('.game--result').text("Winner!");
+    } else if (moves[0] === "X" && moves[3] === "X" && moves[6] === "X") {
+      playerOne = playerOne + 1
+      $('.game--result').text("Player one has won!");
+      $('.playerOne').text(`Player One: ${playerOne}`);
 
-      } else if (moves[6] === "X" && moves[4] === "X" && moves[2] === "X" || moves[6] === "0" && moves[4] === "0" && moves[2] === "0") {
-         $('.game--result').text("Winner!");
-      } else if (markButton === 9) {
-         $('.game--result').text("cat's game! MEOW!");
-      }
+    } else if (moves[0] === "0" && moves[3] === "0" && moves[6] === "0") {
+      playerTwo = playerTwo + 1
+      $('.game--result').text("Player two has won!");
+      $('.playerTwo').text(`Player Two: ${playerTwo}`);
 
-      if ($(".game--result").text().includes("Winner"||"cat's game! MEOW!")) {
+    } else if (moves[1] === "X" && moves[4] === "X" && moves[7] === "X") {
+      playerOne = playerOne + 1
+      $('.game--result').text("Player one has won!");
+      $('.playerOne').text(`Player One: ${playerOne}`);
+
+    } else if (moves[1] === "0" && moves[4] === "0" && moves[7] === "0") {
+      playerTwo = playerTwo + 1
+      $('.game--result').text("Player two has won!");
+        $('.playerTwo').text(`Player Two: ${playerTwo}`);
+
+    } else if (moves[2] === "X" && moves[5] === "X" && moves[8] === "X") {
+      playerOne = playerOne + 1
+      $('.game--result').text("Player one has won!");
+      $('element').text(`Player One: ${playerOne}`);
+
+    } else if (moves[2] === "0" && moves[5] === "0" && moves[8] === "0") {
+      playerTwo = playerTwo + 1
+      $('.game--result').text("Player two has won!");
+      $('.playerTwo').text(`Player Two: ${playerTwo}`);
+
+    } else if (moves[0] === "X" && moves[4] === "X" && moves[8] === "X"){
+      playerOne = playerOne + 1
+      $('.game--result').text("Player one has won!");
+      $('.playerOne').text(`Player One: ${playerOne}`);
+
+    } else if (moves[0] === "0" && moves[4] === "0" && moves[8] === "0") {
+      playerTwo = playerTwo + 1
+      $('.game--result').text("Player two has won!");
+        $('.playerTwo').text(`Player Two: ${playerTwo}`);
+
+    } else if (moves[6] === "X" && moves[4] === "X" && moves[2] === "X") {
+      playerOne = playerOne + 1
+      $('.game--result').text("Player one has won!");
+      $('.playerOne').text(`Player One: ${playerOne}`);
+
+    } else if (moves[6] === "0" && moves[4] === "0" && moves[2] === "0") {
+      playerTwo = playerTwo + 1
+      $('.game--result').text("Player two has won!");
+      $('.playerTwo').text(`Player Two: ${playerTwo}`);
+
+    } else if (markButton === 9) {
+      $('.game--result').text("cat's game! MEOW!");
+    }
+
+    if ($(".game--result").text().includes("Player one has won!"||"Player two has won!"||"cat's game! MEOW!")) {
       $('.button').off("click");
-      $('.game--board').off("click");
-      }
 
+    }
 
-   };
+  };
 
-   $('#restart').click(function() {
-      window.location.reload()
-      // console.log('click');
-      // $('.button').text("");
-      // $('.game--result').text("");
+  $('#restart').on('click', function() {
+    buttonRefresh ();
+    markButton = 0;
+    moves = [];
 
-      // $('.button').on("click");
-      // $('.game--board').on("click");
-      // changePlayer ();
-   })
+    $('.button').html("");
+    $('.game--result').html("");
 
+    currentPlayer = "X"
+  })
 
-   //doc ready close out
+  buttonRefresh();
+
 });
